f13249e8e84b637bccb4630f158eaf9c641a6b65
nagesingh@deloitte.com <nagesingh@deloitte.com>
2018-10-15 19:13:22 +0530

    Initial Commit

--- src/main/java/com/forceCI/forceCI/controller/ForceCIController.java
+++ src/main/java/com/forceCI/forceCI/controller/ForceCIController.java
@@ -80,4 +80,69 @@ public class ForceCIController {
         httpResponse.sendRedirect(null);
 
     }
+
+    @RequestMapping(value = "/webhook", method = RequestMethod.GET, params = {"code", "state"})
+    public void webhookReceiver(@RequestParam String code, @RequestParam String state, ServletResponse response, ServletRequest
+            request) throws Exception {
+
+        String environment = "https://github.com/login/oauth/access_token";
+
+        System.out.println("environment -> " + environment);
+        HttpClient httpClient = new HttpClient();
+
+        PostMethod post = new PostMethod(environment);
+        post.addParameter("code", code);
+        post.addParameter("grant_type", "authorization_code");
+        post.addParameter("redirect_uri", "https://f6278d41.ngrok.io/auth");
+        post.addParameter("client_id", "3MVG9d8..z.hDcPLDlm9QqJ3hRa..IRUJdGRp4Shjuu01GT.H5KRjos_xlbZEtYGy55M6SzOOELg7sfD4T6Pl");
+        post.addParameter("client_secret", "1846517738759045110");
+
+        httpClient.executeMethod(post);
+        String responseBody = post.getResponseBodyAsString();
+
+        String accessToken = null;
+        String issuedAt = null;
+        String signature = null;
+        String id_token = null;
+        String instance_url = null;
+        String useridURL = null;
+        String username = null;
+        String display_name = null;
+        String email = null;
+        JsonParser parser = new JsonParser();
+
+        JsonObject jsonObject = parser.parse(responseBody).getAsJsonObject();
+        System.out.println("jsonObject is now "+jsonObject);
+
+        try {
+
+            accessToken = jsonObject.get("access_token").getAsString();
+            issuedAt = jsonObject.get("issued_at").getAsString();
+            signature = jsonObject.get("signature").getAsString();
+            id_token = jsonObject.get("id_token").getAsString();
+            instance_url = jsonObject.get("instance_url").getAsString();
+            useridURL = jsonObject.get("id").getAsString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            post.releaseConnection();
+        }
+
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        Cookie session1 = new Cookie("ACCESS_TOKEN", accessToken);
+        Cookie session2 = new Cookie("INSTANCE_URL", instance_url);
+        Cookie session3 = new Cookie("ID_TOKEN", id_token);
+        Cookie session4 = new Cookie("USERIDURL", useridURL);
+        session1.setMaxAge(-1); //cookie not persistent, destroyed on browser exit
+        session2.setMaxAge(-1); //cookie not persistent, destroyed on browser exit
+        session3.setMaxAge(-1); //cookie not persistent, destroyed on browser exit
+        session4.setMaxAge(-1); //cookie not persistent, destroyed on browser exit
+        httpResponse.addCookie(session1);
+        httpResponse.addCookie(session2);
+        httpResponse.addCookie(session3);
+        httpResponse.addCookie(session4);
+        httpResponse.sendRedirect(null);
+
+    }
 }
