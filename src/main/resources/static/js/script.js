3ce297ba6fc6f9e6de80ab41b53b8c7b94e8b4db
nagesingh@deloitte.com <nagesingh@deloitte.com>
2018-12-31 15:27:19 +0530

    Repository page change

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -13,7 +13,7 @@ app.controller('orderFromController', function($scope, $http) {
     }
 
     function listRepositoryErrorCallback(error) {}
-    $scope.change = function(obj,$event){
+    $scope.change = function($event){
         console.log($event);
     }
 
