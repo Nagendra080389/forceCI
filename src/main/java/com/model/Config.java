1b02abde753d4c46e325050215bad4fc024c87b9
nagesingh@deloitte.com <nagesingh@deloitte.com>
2019-01-01 15:29:59 +0530

    Repository page change

---
+++ src/main/java/com/model/Config.java
@@ -0,0 +1,57 @@
+
+package com.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "url",
+    "content_type"
+})
+public class Config {
+
+    @JsonProperty("url")
+    private String url;
+    @JsonProperty("content_type")
+    private String contentType;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("url")
+    public String getUrl() {
+        return url;
+    }
+
+    @JsonProperty("url")
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @JsonProperty("content_type")
+    public String getContentType() {
+        return contentType;
+    }
+
+    @JsonProperty("content_type")
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
---
+++ src/main/java/com/model/CreateWebhookPayload.java
@@ -0,0 +1,84 @@
+
+package com.model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "name",
+    "active",
+    "events",
+    "config"
+})
+public class CreateWebhookPayload {
+
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("active")
+    private Boolean active;
+    @JsonProperty("events")
+    private List<String> events = null;
+    @JsonProperty("config")
+    private Config config;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("active")
+    public Boolean getActive() {
+        return active;
+    }
+
+    @JsonProperty("active")
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    @JsonProperty("events")
+    public List<String> getEvents() {
+        return events;
+    }
+
+    @JsonProperty("events")
+    public void setEvents(List<String> events) {
+        this.events = events;
+    }
+
+    @JsonProperty("config")
+    public Config getConfig() {
+        return config;
+    }
+
+    @JsonProperty("config")
+    public void setConfig(Config config) {
+        this.config = config;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
