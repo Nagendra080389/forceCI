5644fa27c58263d1492b92fa7d2d01817df8ea1c
nagesingh@deloitte.com <nagesingh@deloitte.com>
2019-12-07 11:21:48 +0530

    Toast destroy

--- src/main/java/com/ForceCiApplication.java
+++ src/main/java/com/ForceCiApplication.java
@@ -2,8 +2,10 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.dao")
 public class ForceCiApplication {
 
 	public static void main(String[] args) {
