d2b5ae3e203a334a9d6286842ab59db636e97327
U392267 <Nagendra_Singh@bcbsil.com>
2019-12-16 11:41:08 +0530

    Ant runner

--- src/main/resources/build/get_clone.sh
+++ src/main/resources/build/get_clone.sh
@@ -5,7 +5,10 @@ echo $3
 echo $4
 
 echo 'Starting git clone'
-rpm -i xmlstarlet-1.3.0-1.fc16.i686.rpm
+
+sudo apt-get update
+sudo apt-get install xmlstarlet
+
 cd $1 || exit
 
 git config --global user.email $3
