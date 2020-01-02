fc1fbe17de0f31372f774eec7e45ec96c514248a
$userName <$userEmail>
2020-01-02 17:52:17 +0530

    Enable angular routing

--- src/main/java/com/ForceCiApplication.java
+++ src/main/java/com/ForceCiApplication.java
@@ -3,6 +3,7 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -13,27 +14,7 @@ import java.net.URI;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackages = "com.dao")
-@EnableRedisRepositories(basePackages = "com.reddis")
 public class ForceCiApplication {
-
-    @Bean
-    JedisConnectionFactory jedisConnectionFactory(){
-        String redisUriString = System.getenv("REDIS_URL");
-
-        URI redisUri = URI.create(redisUriString);
-        JedisConnectionFactory connectionFactory = new JedisConnectionFactory();
-        connectionFactory.setHostName(redisUri.getHost());
-        connectionFactory.setPort(redisUri.getPort());
-        connectionFactory.setPassword(redisUri.getUserInfo().substring(redisUri.getUserInfo().indexOf(":")+1));
-        return connectionFactory;
-    }
-
-    @Bean
-    RedisTemplate<String, WebSocketSession> redisTemplate(){
-        RedisTemplate<String,WebSocketSession> redisTemplate = new RedisTemplate<String, WebSocketSession>();
-        redisTemplate.setConnectionFactory(jedisConnectionFactory());
-        return redisTemplate;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ForceCiApplication.class, args);
--- src/main/java/com/reddis/RedisWebSocketSessionRepository.java
+++ src/main/java/com/reddis/RedisWebSocketSessionRepository.java
@@ -1,33 +1,42 @@
 package com.reddis;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.socket.WebSocketSession;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Repository
 public class RedisWebSocketSessionRepository {
+    private static final String WEBSOCKETSESSION = "WEBSOCKETSESSION";
+
     private HashOperations hashOperations;
 
-    private RedisTemplate redisTemplate;
+    private RedisTemplate<String, WebSocketSession> redisTemplate;
 
-    public RedisWebSocketSessionRepository(RedisTemplate redisTemplate){
+    @Autowired
+    public RedisWebSocketSessionRepository(RedisTemplate<String, WebSocketSession> redisTemplate){
         this.redisTemplate = redisTemplate;
-        this.hashOperations = this.redisTemplate.opsForHash();
+    }
+
+    @PostConstruct
+    private void init(){
+        hashOperations = redisTemplate.opsForHash();
     }
 
     public void save(WebSocketSession webSocketSession){
         String userName = (String) webSocketSession.getAttributes().get("userName");
-        hashOperations.put("WEBSOCKETSESSION", userName, webSocketSession);
+        hashOperations.put(WEBSOCKETSESSION, userName, webSocketSession);
     }
     public List findAll(){
-        return hashOperations.values("WEBSOCKETSESSION");
+        return hashOperations.values(WEBSOCKETSESSION);
     }
 
     public WebSocketSession findByUserName(String userName){
-        return (WebSocketSession) hashOperations.get("WEBSOCKETSESSION", userName);
+        return (WebSocketSession) hashOperations.get(WEBSOCKETSESSION, userName);
     }
 
     public void update(WebSocketSession webSocketSession){
@@ -35,6 +44,6 @@ public class RedisWebSocketSessionRepository {
     }
 
     public void delete(String userName){
-        hashOperations.delete("WEBSOCKETSESSION", userName);
+        hashOperations.delete(WEBSOCKETSESSION, userName);
     }
 }
