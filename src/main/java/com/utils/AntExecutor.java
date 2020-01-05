3854f83aff8fc583e7c21ad29e4b4879533f293b
$userName <$userEmail>
2020-01-05 17:24:44 +0530

    Refresh Token

--- src/main/java/com/utils/AntExecutor.java
+++ src/main/java/com/utils/AntExecutor.java
@@ -41,7 +41,7 @@ public class AntExecutor {
             }
         }
 
-        /*project.addBuildListener(new BuildListener() {
+        project.addBuildListener(new BuildListener() {
             @Override
             public void buildStarted(BuildEvent buildEvent) {
 
@@ -80,9 +80,7 @@ public class AntExecutor {
                     consoleLogs.add(buildEvent.getMessage());
                 }
             }
-        });*/
-
-        project.addBuildListener(getConsoleLogger());
+        });
 
         // Capture event for Ant script build start / stop / failure
         try {
@@ -101,15 +99,6 @@ public class AntExecutor {
         }
 
         return consoleLogs;
-    }
-
-    private static DefaultLogger getConsoleLogger() {
-        DefaultLogger consoleLogger = new DefaultLogger();
-        consoleLogger.setErrorPrintStream(System.err);
-        consoleLogger.setOutputPrintStream(System.out);
-        consoleLogger.setMessageOutputLevel(Project.MSG_INFO);
-
-        return consoleLogger;
     }
 
 
