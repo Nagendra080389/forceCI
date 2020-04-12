256258a9e80f88826a97c59f70ea2a3c22d167df
nagendra080389 <nagendra080389@gmail.com>
2020-04-12 23:23:52 +0530

    Implement AmazonS3Client

--- src/main/java/com/service/AmazonS3Client.java
+++ src/main/java/com/service/AmazonS3Client.java
@@ -8,11 +8,13 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 
-@Service
+@Configuration
 public class AmazonS3Client {
 
     @Value("${amazons3.region}")
