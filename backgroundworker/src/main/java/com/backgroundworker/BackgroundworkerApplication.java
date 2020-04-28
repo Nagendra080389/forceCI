b9ce80fec40bf812a30fcecf23d9bf1cfd1cd4a4
Nagendra080389 <nagendra080389@gmail.com>
2020-04-28 12:16:44 +0530

    Move RabbitMQ Config to Commons

--- backgroundworker/src/main/java/com/backgroundworker/BackgroundworkerApplication.java
+++ backgroundworker/src/main/java/com/backgroundworker/BackgroundworkerApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.backgroundworker", "com.commonsRabbitMQ"})
 @EnableScheduling
 public class BackgroundworkerApplication {
 
