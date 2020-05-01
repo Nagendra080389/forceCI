c10ec56add44042c06267fe8dfe49085316eab77
Nagendra080389 <nagendra080389@gmail.com>
2020-05-01 11:25:12 +0530

    Tooling API commit

--- webworker/src/main/java/com/utils/SFDCUtils.java
+++ webworker/src/main/java/com/utils/SFDCUtils.java
@@ -1,12 +1,26 @@
 package com.utils;
 
+import com.backgroundworker.quartzJob.ScheduledJobRepositoryCustomImpl;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.model.SFDCConnectionDetails;
 import com.rabbitMQ.DeploymentJob;
 import com.sforce.soap.metadata.CancelDeployResult;
 import com.sforce.soap.metadata.DeployResult;
 import com.sforce.soap.metadata.DeployStatus;
 import com.sforce.soap.metadata.MetadataConnection;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class SFDCUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(SFDCUtils.class);
 
     public static String cancelDeploy(MetadataConnection metadataConnection, DeploymentJob deploymentJob) throws Exception {
 
@@ -40,6 +54,36 @@ public class SFDCUtils {
             System.out.println("Final deploy status = >" + deployResult.getStatus());
             return "Done";
         }
+    }
+
+    public static String refreshSFDCToken(SFDCConnectionDetails sfdcConnectionDetails) throws IOException {
+        String refreshToken = sfdcConnectionDetails.getRefreshToken();
+        String environment = sfdcConnectionDetails.getEnvironment();
+        String instanceURL = sfdcConnectionDetails.getInstanceURL();
+        String clientId = System.getenv("SFDC_CLIENTID");
+        String clientSecret = System.getenv("SFDC_CLIENTSECRET");
+        String url = "";
+        if (environment.equals("0")) {
+            url = "https://login.salesforce.com/services/oauth2/token?" +
+                    "grant_type=refresh_token&client_id=" + clientId + "&client_secret=" + clientSecret +
+                    "&refresh_token=" + refreshToken;
+        } else if (environment.equals("1")) {
+            url = "https://test.salesforce.com/services/oauth2/token?" +
+                    "grant_type=refresh_token&client_id=" + clientId + "&client_secret=" + clientSecret +
+                    "&refresh_token=" + refreshToken;
+        } else {
+            url = instanceURL + "/services/oauth2/token?" +
+                    "grant_type=refresh_token&client_id=" + clientId + "&client_secret=" + clientSecret +
+                    "&refresh_token=" + refreshToken;
+        }
+        HttpClient httpClient = new HttpClient();
+        PostMethod post = new PostMethod(url);
+        int intStatus = httpClient.executeMethod(post);
+        logger.info("Refresh Token return status = "+intStatus);
+        String responseBody = IOUtils.toString(post.getResponseBodyAsStream(), StandardCharsets.UTF_8);
+        JsonParser parser = new JsonParser();
+        JsonObject jsonObject = parser.parse(responseBody).getAsJsonObject();
+        return jsonObject.get("access_token").getAsString();
     }
 
 }
